c96ec931a2c18e2c7d893b592240474369fbf780
Amedeo Zucchetti <amedeo.zucchetti@welld.ch>
2019-11-17 13:45:30 +0100

    v1

---
+++ precache-manifest.b9da17a5d12dfad500692cf799f1c0db.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae336c8d87cd220f06ccba4a1c2de91e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2bab8e7ed9c80f74bf5b",
+    "url": "./static/css/main.3b13b6ae.chunk.css"
+  },
+  {
+    "revision": "8e10110a8fb95435e979",
+    "url": "./static/js/2.ff37d66f.chunk.js"
+  },
+  {
+    "revision": "2bab8e7ed9c80f74bf5b",
+    "url": "./static/js/main.399df490.chunk.js"
+  },
+  {
+    "revision": "c83efcaa03e6a2438968",
+    "url": "./static/js/runtime-main.35df8444.js"
+  },
+  {
+    "revision": "36a26b88597b7090c51240be48e1a2ef",
+    "url": "./static/media/position_marker.36a26b88.svg"
+  },
+  {
+    "revision": "3dfb6805f066a73e7a0cd9adf388a1a4",
+    "url": "./static/media/station_marker.3dfb6805.svg"
+  },
+  {
+    "revision": "2af14da7e92f55aace7128541780a1ad",
+    "url": "./static/media/station_marker_hot.2af14da7.svg"
+  }
+]);
